1f00f6a0492d4c95aede8897e43c7a4415fcab67
dsankov <71551209+dsankov@users.noreply.github.com>
2024-05-06 19:01:45 +0300

    Time: 66 ms (45.35%), Space: 18.1 MB (6.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        tested_nums = {}
-        for index, number in enumerate(nums):
-            if target - number in tested_nums:
-                return (index, tested_nums[target - number])
-            else:
-                tested_nums[number] = index
-        
-        return [] # error case
+        n = len(nums)
+        differences = {}
+        for i in range(n):
+            x = nums[i]
+            if x in differences:
+                return  i, differences[x]
+            differences[target-x] = i
