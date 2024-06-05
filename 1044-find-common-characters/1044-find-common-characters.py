9e8674491754906d72df2854ac779766ca452783
dsankov <71551209+dsankov@users.noreply.github.com>
2024-06-05 23:49:23 +0300

    Time: 61 ms (9.64%), Space: 16.7 MB (66.63%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.py
+++ 1044-find-common-characters/1044-find-common-characters.py
@@ -2,22 +2,18 @@ class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
         n = len(words)
         letters = {}
-        result = defaultdict(int)
+        common_letters = defaultdict(int)
         for word in words:
             letters[word] = Counter(word)
-        # print(letters)
         for letter in letters[words[0]]:
-            result[letter] = letters[words[0]][letter]
-            # print(letter)
+            common_letters[letter] = letters[words[0]][letter]
             for word in words:
                 if letter in letters[word]:
-                    # print(letters[words[0]][letter], letters[word][letter])
-                    result[letter] = min(result[letter], letters[word][letter])
+                    common_letters[letter] = min(common_letters[letter], letters[word][letter])
                 else:
-                    result[letter] = 0
+                    common_letters[letter] = 0
 
-        # print(result)
-        answer = []
-        for letter, number in result.items():
-            answer.extend(letter * number)
-        return answer
+        result = []
+        for letter, number in common_letters.items():
+            result.extend(letter * number)
+        return result
