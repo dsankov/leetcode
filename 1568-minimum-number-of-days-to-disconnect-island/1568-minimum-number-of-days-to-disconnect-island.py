cd0031164044ed4282ff99df37615a5cd9564f21
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-08-13 15:31:25 +0300

    Time: 264 ms (88.39%), Space: 28.9 MB (5.02%) - LeetHub

--- 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.py
+++ 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+import numpy as np
 class Solution:
     DIRECTIONS = [[0,1], [1,0], [0,-1],[-1,0]]
     
@@ -23,19 +25,17 @@ class Solution:
                 self.find_articulation_point(child_row, child_col)
 
                 self.lowest_reachable_time[row][col] = min(
-                    self.lowest_reachable_time[row][col],
-                    self.lowest_reachable_time[child_row][child_col]
-                )
-
+                                                        self.lowest_reachable_time[row][col],
+                                                        self.lowest_reachable_time[child_row][child_col])
+                
                 if (self.lowest_reachable_time[child_row][child_col] >= self.discovery_time[row][col]
                     and self.parent_cell[row][col] != -1):
-                    print(f"{row=} {col=} {child_row=}  {child_col=}")
-                    self.has_AP = True
+                        self.has_AP = True
+
             elif child_row * self.cols + child_col != self.parent_cell[row][col] :
-                self.lowest_reachable_time[row][col] >= min(
-                    self.lowest_reachable_time[row][col],
-                    self.discovery_time[child_row][child_col],
-                )
+                self.lowest_reachable_time[row][col] = min(
+                                                        self.lowest_reachable_time[row][col],
+                                                        self.discovery_time[child_row][child_col])
         
         if (self.parent_cell[row][col] == -1
             and discovered_children_number > 1) :
@@ -60,9 +60,7 @@ class Solution:
                     count_land_cells += 1
                     if self.discovery_time[row][col] == -1:
                         self.find_articulation_point(row, col)
-                        print(self.discovery_time)
                         count_islands += 1
-        print (self.has_AP, count_land_cells, count_islands, self.time)
         if count_islands == 0 or count_islands >= 2:
             return 0
         if count_land_cells == 1:
