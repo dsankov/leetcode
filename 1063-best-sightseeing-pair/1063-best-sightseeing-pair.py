6bfe3edbe685ba418f080eaf95edbafea0b262e1
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-08-19 20:22:44 +0300

    Time: 322 ms (94.42%), Space: 22.1 MB (90.52%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        n = len(values)
-        max_score = values.copy()
-        max_from_left = [-inf] * len(values)
-        for i in range(1, n):
-            max_from_left[i] = max(max_from_left[i-1] - 1, values[i-1] - 1)
-        max_from_right = [-inf] * len(values)
-        for i in range(n-2, -1, -1):
-            max_from_right[i] = max(max_from_right[i+1] - 1, values[i+1] - 1)
-        
-        max_score = -inf
-        return max(score + max(left_score, right_score) for left_score, score, right_score in zip(max_from_left, values, max_from_right))
-
- 
+        best_pair = 0
+        answer = 0
+        for sight in values:
+            answer = max(answer, sight + best_pair)
+            if sight > best_pair: 
+                best_pair = sight - 1
+            else: 
+                best_pair -= 1
+        return answer
+# with open("user.out", "w") as f:
+#     for nums in map(loads, stdin):
+#         print(Solution().maxScoreSightseeingPair(nums),file=f)
+# exit(0)
