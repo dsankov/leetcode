cd731aa7420507cc8dc1261003fad55be87cf07d
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-09-29 15:37:40 +0300

    Time: 145 ms (96.58%), Space: 17.4 MB (16.06%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,14 +1,33 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word =False
+
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         n = len(s)
-        dictionary_set = set(dictionary)
+        root = self._build_trie(dictionary)
         dp = [0] * (len(s) + 1)
 
         for start in range(n - 1, -1, -1):
             dp[start] = 1 + dp[start + 1]
+            node = root
             for end in range(start, n):
-                curr = s[start: end + 1]
-                if curr in dictionary_set:
+                if s[end] not in node.children:
+                    break
+                node = node.children[s[end]]
+                if node.is_word:
                     dp[start] = min(dp[start], dp[end + 1])
 
         return dp[0]
+
+    def _build_trie(self, dictionary):
+        root = TrieNode()
+        for word in dictionary:
+            node =root
+            for char in word:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+            node.is_word = True
+        return root
