ffaafda9ca05189cfb0c6f213cb87cff32a68d57
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-10-02 11:38:01 +0000

    Time: 215 ms (99.92%), Space: 41.1 MB (5.43%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,4 +1,4 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        return [dict(zip(sorted(set(arr)), count(1)))[num] for num in arr]
+        return map(dict(zip(sorted(set(arr)), count(1))).get, arr)
         
