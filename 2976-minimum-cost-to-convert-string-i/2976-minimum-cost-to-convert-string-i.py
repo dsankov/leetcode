c66a0018c7f8f2b273f67ac80f9183195e981c05
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-10-17 18:18:13 +0300

    Time: 945 ms (83.74%), Space: 18.4 MB (49.16%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
@@ -7,31 +7,27 @@ class Solution:
         changed: List[str],
         cost: List[int],
     ) -> int:
-        source = [ord(letter) - ord("a") for letter in source]
-        target = [ord(letter) - ord("a") for letter in target]
-        original = [ord(letter) - ord("a") for letter in original]
-        changed = [ord(letter) - ord("a") for letter in changed]
 
-        min_cost = [ [inf] * 26 for _ in range(26) ]
+        min_cost_matrix = defaultdict(lambda: inf)
         for source_vertex, dest_vertex, change_cost in zip(original, changed, cost):
-            min_cost[source_vertex][dest_vertex] = min(
-                min_cost[source_vertex][dest_vertex],
+            min_cost_matrix[source_vertex, dest_vertex] = min(
+                min_cost_matrix[source_vertex, dest_vertex],
                 change_cost
             )
-        for intermediate_vertex in range(26):
-            for source_vertex in range(26):
-                for dest_vertex in range(26):
-                    min_cost[source_vertex][dest_vertex] = min(
-                        min_cost[source_vertex][dest_vertex],
-                        min_cost[source_vertex][intermediate_vertex] + min_cost[intermediate_vertex][dest_vertex]
+        used_letters = list(set(original + changed))
+        for intermediate_vertex in used_letters:
+            min_cost_matrix[intermediate_vertex, intermediate_vertex] = 0
+            for source_vertex in used_letters:
+                for dest_vertex in used_letters:
+                    min_cost_matrix[source_vertex, dest_vertex] = min(
+                        min_cost_matrix[source_vertex, dest_vertex],
+                        min_cost_matrix[source_vertex, intermediate_vertex] + min_cost_matrix[intermediate_vertex, dest_vertex]
             )       
 
         total_cost = 0
         for source_letter, target_letter in zip(source, target):
-            if source_letter == target_letter:
-                continue
-            if min_cost[source_letter][target_letter] == inf:
+            if min_cost_matrix[source_letter, target_letter] == inf:
                 return -1
-            total_cost += min_cost[source_letter][target_letter]
+            total_cost += min_cost_matrix[source_letter, target_letter]
 
         return total_cost
