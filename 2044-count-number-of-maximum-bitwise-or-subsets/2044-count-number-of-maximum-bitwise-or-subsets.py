75bb32992ff55908373561d6c9c98aee78fad1cd
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-10-18 19:51:53 +0300

    Time: 199 ms (77.39%), Space: 33.8 MB (7.84%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -6,15 +6,15 @@ class Solution:
         for num in nums:
             self.total_or |= num
         self.UNKNOWN = -1
-        self.computed_results = defaultdict(lambda: self.UNKNOWN)
+        self.computed_results = [[self.UNKNOWN] * (self.total_or + 1) for _ in range(self.n + 1)]
         return self.max_or_subset(0, 0)
     def max_or_subset(self, num_index: int, current_or: int) -> int:
         if num_index >= self.n:
             return 1 if current_or == self.total_or else 0
-        if self.computed_results[(index, current_or)] != self.UNKNOWN:
-            return self.computed_results[(index, dest_or)]
+        if self.computed_results[num_index][current_or] != self.UNKNOWN:
+            return self.computed_results[num_index][current_or]
         with_or = self.max_or_subset(num_index+1, current_or | self.nums[num_index])
         without_or = self.max_or_subset(num_index+1, current_or)
-        self.computed_results[(num_index, current_or)] = with_or + without_or
+        self.computed_results[num_index][current_or] = with_or + without_or
         return with_or + without_or
         
