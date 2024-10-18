dc786f5dd5614baffd769f985e53e02225bb8abf
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-10-18 19:52:21 +0300

    Time: 199 ms (77.39%), Space: 33.8 MB (7.84%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets-2d_list_memo.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        self.n = len(nums)
+        self.nums = nums
+        self.total_or = 0
+        for num in nums:
+            self.total_or |= num
+        self.UNKNOWN = -1
+        self.computed_results = [[self.UNKNOWN] * (self.total_or + 1) for _ in range(self.n + 1)]
+        return self.max_or_subset(0, 0)
+    def max_or_subset(self, num_index: int, current_or: int) -> int:
+        if num_index >= self.n:
+            return 1 if current_or == self.total_or else 0
+        if self.computed_results[num_index][current_or] != self.UNKNOWN:
+            return self.computed_results[num_index][current_or]
+        with_or = self.max_or_subset(num_index+1, current_or | self.nums[num_index])
+        without_or = self.max_or_subset(num_index+1, current_or)
+        self.computed_results[num_index][current_or] = with_or + without_or
+        return with_or + without_or
+        
