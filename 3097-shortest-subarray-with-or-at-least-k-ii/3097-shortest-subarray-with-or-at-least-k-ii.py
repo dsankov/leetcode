cd8c6fa201a09920154b9c535ecf0fd26cc531e1
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-11-10 12:21:23 +0300

    Time: 1091 ms (72.09%), Space: 35.7 MB (72.93%) - LeetHub

--- 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.py
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.py
@@ -3,7 +3,8 @@ class Solution:
         if k == 0:
             return 1
 
-        MAX_BITS = ceil(log2(10**9)) + 1
+        # MAX_BITS = ceil(log2(10**9)) + 1
+        MAX_BITS = 30
         NEGATIVE_BIT = (2 ** MAX_BITS) - 1
 
         n = len(nums)
@@ -11,16 +12,8 @@ class Solution:
         sliding_or = 0
         sliding_or_bits = [0 for _ in range(MAX_BITS)]
         min_length = math.inf
-        while left < n:
-            if sliding_or < k:
-                if right >= n:
-                    break
-                for bit_index in range(MAX_BITS):
-                    if nums[right] & (1 << bit_index):
-                        sliding_or_bits[bit_index] += 1
-                sliding_or |= nums[right]
-                right += 1
-            else:
+        while right < n:
+            while sliding_or >= k:
                 min_length = min(min_length, right - left)
                 if min_length == 1:
                     return min_length
@@ -30,5 +23,22 @@ class Solution:
                         if sliding_or_bits[bit_index] == 0:
                             sliding_or &= NEGATIVE_BIT ^ (1 << bit_index)
                 left += 1
+            
+            for bit_index in range(MAX_BITS):
+                if nums[right] & (1 << bit_index):
+                    sliding_or_bits[bit_index] += 1
+            sliding_or |= nums[right]
+            right += 1
+
+        while sliding_or >= k:
+            min_length = min(min_length, right - left)
+            if min_length == 1:
+                return min_length
+            for bit_index in range(MAX_BITS):
+                if nums[left] & (1 << bit_index):
+                    sliding_or_bits[bit_index] -= 1
+                    if sliding_or_bits[bit_index] == 0:
+                        sliding_or &= NEGATIVE_BIT ^ (1 << bit_index)
+            left += 1
 
         return min_length if min_length != math.inf else -1
