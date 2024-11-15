26e7e2254926ac7b05f6ce17e31506d8689e2502
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-11-15 18:09:59 +0300

    Time: 15 ms (58.56%), Space: 30.2 MB (94.7%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -21,13 +21,13 @@ class Solution:
         min_length_of_mid_part = min(n - 1 - end_of_start_sorted_part, start_of_end_sorted_part)
         if min_length_of_mid_part == 0:
             return 0
-
+        left = 0
         right = start_of_end_sorted_part
-        for left in range(end_of_start_sorted_part + 1):
+        while left <= end_of_start_sorted_part and right < n:
             while right < n and arr[left] > arr[right]:
-
-                right += 1
+                right += 1                
             min_length_of_mid_part = min(min_length_of_mid_part,
                                          right - 1 - left)
+            left += 1
 
         return min_length_of_mid_part        
