bdd9343395a97933cb63d966a465855d5baebc89
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-11-20 15:34:44 +0300

    Time: 189 ms (77.22%), Space: 17.2 MB (79.81%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
@@ -1,19 +1,18 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
         ALLOWED_CHARS = "abc"
-        chars_counter = Counter(s)
-        if any(chars_counter[char] < k for char in ALLOWED_CHARS):
+        total_chars_counter = Counter(s)
+        if any(total_chars_counter[char] < k for char in ALLOWED_CHARS):
             return -1
-            
+
         n = len(s)
         left = 0
         curr_window_chars_counter = defaultdict(int)
         max_window_size = 0
-        for right in range(n):
-            curr_window_chars_counter[s[right]] += 1
+        for right, char in enumerate(s):
+            curr_window_chars_counter[char] += 1
             while (left <= right
-                   and any(chars_counter[char] - curr_window_chars_counter[char] < k
-                            for char in ALLOWED_CHARS)
+                   and total_chars_counter[char] - curr_window_chars_counter[char] < k
             ):
                 curr_window_chars_counter[s[left]] -= 1
                 left += 1
