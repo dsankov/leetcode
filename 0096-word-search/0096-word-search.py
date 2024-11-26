7e2929955e77310adbe02add385b6fce43fbb988
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-11-26 18:29:36 +0300

    Time: 0 ms (100%), Space: 17.4 MB (10.3%) - LeetHub

---
+++ 0096-word-search/0096-word-search.py
@@ -0,0 +1,40 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        board_chars_freqs = Counter(chain.from_iterable(board))
+        for char, freq in Counter(word).items():
+            if freq > board_chars_freqs[char]:
+                return False
+        
+        if board_chars_freqs[word[0]] > board_chars_freqs[word[-1]]:
+            word = word[::-1]
+
+        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        m = len(board)
+        n = len(board[0])
+        used_cells = [[False] * n for _ in range(m)]
+    
+        
+        def backtrack(board, word, used_cells, row, col, word_ptr):
+            if word_ptr == len(word):
+                return True
+            if board[row][col] != word[word_ptr]:
+                return False
+            if word_ptr == len(word) - 1:
+                return True
+
+            used_cells[row][col] = True
+            for d_row, d_col in directions:
+                if 0 <= row + d_row < m and 0 <= col + d_col < n and not used_cells[row + d_row][col + d_col]:
+                    if backtrack(board, word, used_cells, row + d_row, col + d_col, word_ptr + 1):
+                        return True
+            used_cells[row][col] = False
+            return False
+        
+        for start_row in range(m):
+            for start_col in range(n):
+                if backtrack(board, word, used_cells, start_row, start_col, word_ptr=0):
+                    return True
+        return False
+
+        
