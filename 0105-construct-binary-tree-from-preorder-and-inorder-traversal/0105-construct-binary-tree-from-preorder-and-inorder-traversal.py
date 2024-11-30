95490a530c57f7dce3b337366747446e86eada03
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-11-30 23:32:35 +0300

    Time: 2 ms (93.41%), Space: 18.9 MB (68.21%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,6 +7,7 @@
 from pprint import pp, pprint
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        inorder_index_of = {value: index for index, value in enumerate(inorder)}
         
         def build_node(pre_start, in_start, tree_size):
             if tree_size == 0:
@@ -14,16 +15,20 @@ class Solution:
 
             pre_root_index = pre_start
             root_value = preorder[pre_root_index]
-            in_root_index = inorder.index(root_value, in_start)
+            in_root_index = inorder_index_of[root_value]
 
             left_subtree_size = in_root_index - in_start
             right_subtree_size = tree_size - 1 - left_subtree_size
             root_node = TreeNode(root_value)
             
-            root_node.left = build_node(pre_root_index + 1, in_root_index - left_subtree_size, left_subtree_size)
-            root_node.right = build_node(pre_root_index + 1 + left_subtree_size, in_root_index + 1, right_subtree_size)
+            root_node.left = build_node(pre_start=pre_root_index + 1, 
+                                        in_start=in_root_index - left_subtree_size, 
+                                        tree_size=left_subtree_size)
+            root_node.right = build_node(pre_start=pre_root_index + 1 + left_subtree_size, 
+                                         in_start=in_root_index + 1, 
+                                         tree_size=right_subtree_size)
             return root_node
             
-        root = build_node(0, 0, len(preorder))
+        root = build_node(pre_start=0, in_start=0, tree_size=len(preorder))
         return root
         
