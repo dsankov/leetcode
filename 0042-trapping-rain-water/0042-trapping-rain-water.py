6eab87a439ab3aa544bf22186b9dfa63c6268e90
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2024-12-14 18:14:38 +0000

    Time: 15 ms (54.48%), Space: 18.7 MB (31.39%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,29 +1,17 @@
 class Solution:
     def trap(self, heights: List[int]) -> int:
-        height_stack = []
-        height_stack.append(heights[0])
+
+        left, right = 0, len(heights) - 1
+        left_max, right_max = heights[left], heights[right]
         total_water_amount = 0
-
-
-        for height in heights[1:]:
-            curr_water_amount = 0
-            occupied_cells = 0 
-            curr_gap_width = 0
-            prev_height = 0
-            
-            while height_stack and height_stack[-1] <= height:
-                prev_height = height_stack.pop()
-                occupied_cells += prev_height
-                curr_gap_width += 1
-
-            if height_stack:
-                height_stack.extend([height] * curr_gap_width)
-                curr_gap_height = height
+        while left <= right:            
+            if left_max < right_max:
+                left_max = max(left_max, heights[left])
+                total_water_amount += left_max - heights[left]
+                left += 1
             else:
-                curr_gap_height = prev_height
-            height_stack.append(height)
-
-            curr_water_amount = curr_gap_width * curr_gap_height - occupied_cells
-            total_water_amount += curr_water_amount
+                right_max = max(right_max, heights[right])
+                total_water_amount += right_max - heights[right]
+                right -= 1
 
         return total_water_amount
