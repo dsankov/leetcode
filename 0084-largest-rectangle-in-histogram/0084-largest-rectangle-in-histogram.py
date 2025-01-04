4d77cd7f399b89a7b9f065af979fb868cb85412e
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2025-01-04 12:18:57 +0300

    Time: 155 ms (61.96%), Space: 29.2 MB (76.63%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -4,19 +4,13 @@ class Solution:
         max_area = 0
         for curr_idx, curr_height in enumerate(heights):
             right_max_idx = curr_idx
-            curr_max_area = 0
-            while heights_ascending_queue:
-                if heights_ascending_queue[-1][1] < curr_height:
-                    break
+            while heights_ascending_queue and heights_ascending_queue[-1][1] >= curr_height:
                 prev_max_idx, prev_max_height = heights_ascending_queue.pop()
-                curr_max_area = max(curr_max_area, prev_max_height * (curr_idx - prev_max_idx))
+                max_area = max(max_area, prev_max_height * (curr_idx - prev_max_idx))
                 right_max_idx = prev_max_idx
 
-            curr_max_area = max(curr_max_area, curr_height * (curr_idx - right_max_idx + 1))
-
             heights_ascending_queue.append((right_max_idx, curr_height))
-            max_area = max(max_area, curr_max_area)
-        
+            
         n = len(heights)
         while heights_ascending_queue:
             left_min_idx, left_min_height = heights_ascending_queue.popleft()
