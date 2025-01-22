94e0cadcb306dcc0bea8b1c4f0c788f6b19c6c2d
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2025-01-22 14:05:19 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.py
@@ -0,0 +1,32 @@
+from pprint import pp
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        UNVISITED = -1
+        DIRECTIONS = ((0, 1), (0, -1), (-1, 0), (1, 0))
+        num_rows, num_cols = len(isWater), len(isWater[0])
+        cells_height = [[UNVISITED] * num_cols for _ in range(num_rows)]
+        cells_queue = deque()
+        cur_level = 0
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if isWater[row][col] == 1:
+                    cells_height[row][col] = cur_level
+                    cells_queue.append((row, col))
+        next_level = cur_level + 1
+        while cells_queue:
+            pp(cells_height)            
+            pp(cells_queue)
+            level_size = len(cells_queue)
+            for _ in range(level_size):
+                cur_row, cur_col = cells_queue.popleft()
+                print(cur_row, cur_col)
+                for d_row, d_col in DIRECTIONS:
+                    new_row, new_col = cur_row + d_row, cur_col + d_col
+                    if 0 <= new_row < num_rows and 0 <= new_col < num_cols:
+                        print(new_row, new_col)
+                        if cells_height[new_row][new_col] == UNVISITED:
+                            cells_height[new_row][new_col] = next_level
+                            cells_queue.append((new_row, new_col))
+            next_level += 1
+        return cells_height
+        
