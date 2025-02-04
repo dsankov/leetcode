5dbd7a74cc4fc1ebbc9df8b2d2b7eff2296491ca
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2025-02-04 15:37:50 +0300

    Time: 52 ms (86.3%), Space: 41.3 MB (86.31%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,37 +1,26 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: list[list[int]]) -> str:
         n = len(s)
-        diff_array = [
-            0
-        ] * n  # Initialize a difference array with all elements set to 0
+        diff_array = [0] * n  
 
-        # Process each shift operation
-        for shift in shifts:
-            if shift[2] == 1:  # If direction is forward (1)
-                diff_array[shift[0]] += 1  # Increment at the start index
-                if shift[1] + 1 < n:
-                    diff_array[
-                        shift[1] + 1
-                    ] -= 1  # Decrement at the end+1 index
-            else:  # If direction is backward (0)
-                diff_array[shift[0]] -= 1  # Decrement at the start index
-                if shift[1] + 1 < n:
-                    diff_array[
-                        shift[1] + 1
-                    ] += 1  # Increment at the end+1 index
+        for start, end, direction in shifts:
+            if direction == 1:
+                diff_array[start] += 1
+                if end < n - 1:
+                    diff_array[end + 1] -= 1  
+            else: 
+                diff_array[start] -= 1  
+                if end < n - 1:
+                    diff_array[end + 1] += 1
 
-        result = list(s)
+        result = [""] * n
         number_of_shifts = 0
 
-        # Apply the shifts to the string
         for i in range(n):
             number_of_shifts = (
                 number_of_shifts + diff_array[i]
-            ) % 26  # Update cumulative shifts, keeping within the alphabet range
-            if number_of_shifts < 0:
-                number_of_shifts += 26  # Ensure non-negative shifts
+            ) % 26  
 
-            # Calculate the new character by shifting `s[i]`
             shifted_char = chr(
                 (ord(s[i]) - ord("a") + number_of_shifts) % 26 + ord("a")
             )
