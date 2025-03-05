81e000c57169693d7ee2220e9fb982fc62fc0fa8
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2025-03-05 15:26:04 +0300

    2649-COUNT-TOTAL-NUMBER-OF-COLORED-CELLS Stats: Time: 0 ms (100%), Space: 17.8 MB (30.45%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -1,14 +1,8 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        if n == 0:
-            return 0
-        iteration = 1
-        num_cells = 1
-        while iteration < n:
-            num_cells += 4 * iteration
-            iteration += 1
-        return num_cells
+        return 1 + n * (n - 1) * 2
 
+        
 
 
         
