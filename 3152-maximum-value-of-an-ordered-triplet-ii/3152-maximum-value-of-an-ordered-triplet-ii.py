a8d959d40e77e2c69f64c6ac1919a594cd9a2d89
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2025-04-03 09:35:34 +0300

    3152-MAXIMUM-VALUE-OF-AN-ORDERED-TRIPLET-II Stats: Time: 159 ms (84.64%), Space: 29.4 MB (87.03%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.py
@@ -1,18 +1,12 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        n = len(nums)
-        prefix_max = [0] * n
-        suffix_max = [0] * n
-        prefix_max[0] = nums[0]
-        suffix_max[-1] = nums[-1]
-        for beg_idx in range(1, n):
-            end_idx = n - 1 - beg_idx
-            prefix_max[beg_idx] = max(prefix_max[beg_idx - 1], nums[beg_idx])
-            suffix_max[end_idx] = max(suffix_max[end_idx + 1], nums[end_idx])
-        
+        max_num = 0
+        max_diff = 0
         max_value = 0
-        for mid_idx in range(1, n-1):
-            max_value = max(max_value, (prefix_max[mid_idx - 1] - nums[mid_idx]) * suffix_max[mid_idx + 1])
+        for num in nums:
+            max_value = max(max_value, max_diff * num)
+            max_diff = max(max_diff, max_num - num)
+            max_num = max(max_num, num)
 
         return max_value
         
