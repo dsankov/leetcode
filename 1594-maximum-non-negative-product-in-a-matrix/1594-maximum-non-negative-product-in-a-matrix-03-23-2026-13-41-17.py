0b5fee9500bb1cd3386f3b770cc09c9eb4b27324
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2026-03-23 13:41:17 +0300

    Time: 0 ms (100%), Space: 19.3 MB (88.6%) - LeetHub

---
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix-03-23-2026-13-41-17.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProductPath(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        pos_dp = [[-1] * (n + 1) for _ in range(m + 1)]
+        neg_dp = [[-1] * (n + 1) for _ in range(m + 1)]
+
+        pos_dp[0][1] = 1
+
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] >= 0:
+                    pos_dp[row + 1][col + 1] = grid[row][col] * max(
+                        pos_dp[row][col + 1], pos_dp[row + 1][col]
+                    )
+                    neg_dp[row + 1][col + 1] = grid[row][col] * max(
+                        neg_dp[row][col + 1], neg_dp[row + 1][col]
+                    )
+                else:
+                    neg_dp[row + 1][col + 1] = -grid[row][col] * max(
+                        pos_dp[row][col + 1], pos_dp[row + 1][col]
+                    )
+                    pos_dp[row + 1][col + 1] = -grid[row][col] * max(
+                        neg_dp[row][col + 1], neg_dp[row + 1][col]
+                    )
+        return pos_dp[m][n] % (10**9 + 7) if pos_dp[m][n] >= 0 else -1
