f00c53549715e9a7c1aa32568cc568e637b30b39
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2026-04-21 13:27:00 +0300

    Time: 661 ms (5.26%), Space: 142 MB (6.01%) - LeetHub

---
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations-04-21-2026-13-27-00.py
@@ -0,0 +1,57 @@
+class DSU:
+    def __init__(self, n):
+        self.roots = list(range(n))
+        self.ranks = [1] * n
+        self.group_of = {idx: {idx} for idx in range(n)}
+
+    def root_of(self, idx):
+        if self.roots[idx] == idx:
+            return idx
+        root = self.root_of(self.roots[idx])
+        self.roots[idx] = root
+        self.group_of[root].add(idx)
+        return root
+
+    def union(self, a, b):
+        root_a = self.root_of(a)
+        root_b = self.root_of(b)
+        if root_a == root_b:
+            return
+        if self.ranks[root_a] >= self.ranks[root_b]:
+            self.ranks[root_a] += self.ranks[root_b]
+            self.roots[root_b] = self.roots[root_a]
+            self.group_of[root_a] |= self.group_of[root_b]
+            # self.group_of[root_b] = self.group_of[root_a]
+        else:
+            self.ranks[root_b] += self.ranks[root_a]
+            self.roots[root_a] = self.roots[root_b]
+            self.group_of[root_b] |= self.group_of[root_a]
+            # self.group_of[root_a] = self.group_of[root_b]
+
+class Solution:
+    def minimumHammingDistance(self, source: List[int], target: List[int], allowedSwaps: List[List[int]]) -> int:
+        graph = defaultdict(set)
+        for a, b in allowedSwaps:
+            graph[a].add(b)
+            graph[b].add(a)
+
+        n = len(source)
+        groups = DSU(n)
+        for a, swaps in graph.items():
+            for b in swaps:
+                groups.union(a, b)
+
+        distance = 0
+
+        for idx in range(n):
+            if groups.roots[idx] != idx:
+                continue
+
+            cur_group = groups.group_of[idx] 
+            source_cnt = Counter(source[swap] for swap in cur_group)
+            target_cnt = Counter(target[swap] for swap in cur_group)
+            for val, count in target_cnt.items():
+                distance += max(0, count - source_cnt[val])
+
+        return distance
+
