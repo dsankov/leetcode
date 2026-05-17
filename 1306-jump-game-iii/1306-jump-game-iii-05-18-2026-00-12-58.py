e0c123fd27640c925f26857a81511e2d2c07d979
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2026-05-18 00:12:58 +0300

    Time: 7 ms (97.33%), Space: 25.1 MB (92.69%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii-05-18-2026-00-12-58.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        n = len(arr)
+        visited = [False] * n
+        idx_queue = []
+        idx_queue.append(start)
+
+        while idx_queue:
+            cur_idx = idx_queue.pop()
+            if visited[cur_idx]:
+                continue
+            if arr[cur_idx] == 0:
+                return True
+            visited[cur_idx] = True
+            if cur_idx - arr[cur_idx] >= 0:
+                idx_queue.append(cur_idx - arr[cur_idx])
+            if cur_idx + arr[cur_idx] < n:
+                idx_queue.append(cur_idx + arr[cur_idx])
+        return False
