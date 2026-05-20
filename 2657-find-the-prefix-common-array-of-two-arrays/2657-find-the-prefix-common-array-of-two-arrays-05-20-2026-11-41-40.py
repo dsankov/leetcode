09495d8ce13258ad348ddd0cd54cb46be3507ad5
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2026-05-20 11:41:40 +0300

    Time: 4 ms (79.26%), Space: 19.2 MB (93.97%) - LeetHub

---
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays-05-20-2026-11-41-40.py
@@ -0,0 +1,27 @@
+# class Solution:
+#     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+#         n = len(A)
+#         a_seen = set()
+#         b_seen = set()
+#         C = []
+#         for a, b in zip(A, B):
+#             a_seen.add(a)
+#             b_seen.add(b)
+#             C.append(len(a_seen & b_seen))
+#         return C
+        
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        seenA = set()
+        seenB = set()
+        res = []
+        cnt = 0
+        for i in range(len(A)):
+            if A[i] in seenB:
+                cnt += 1
+            seenA.add(A[i])
+            if B[i] in seenA:
+                cnt += 1
+            seenB.add(B[i])
+            res.append(cnt)
+        return res
