e98656359210837990ac0bd3075172ee4f1183ea
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2026-05-21 16:07:57 +0300

    Time: 462 ms (17%), Space: 38.7 MB (13.49%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix-05-21-2026-16-07-56.py
@@ -0,0 +1,27 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 10 
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        if len(arr1) < len(arr2):
+            arr1, arr2 = arr2, arr1
+        root_1 = TrieNode()
+        for num in arr1:
+            cur_node = root_1
+            for digit in map(int, str(num)):
+                if not cur_node.children[digit]:
+                    cur_node.children[digit] = TrieNode()
+                cur_node = cur_node.children[digit]
+        max_prefix_len = 0
+        for num in arr2:
+            cur_node = root_1
+            cur_prefix_len = 0
+            for digit in map(int, str(num)):
+                if not cur_node.children[digit]:
+                    break
+                cur_prefix_len += 1
+                cur_node = cur_node.children[digit]
+            max_prefix_len = max(max_prefix_len, cur_prefix_len)
+
+        return max_prefix_len
+        
