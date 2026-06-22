9a9e805a526bca26c92c9ac57bb08811560f6727
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2026-06-22 17:17:36 +0300

    Time: 79 ms (55.93%), Space: 25.2 MB (50.68%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,19 +1,16 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        prefix_ones = [nums[0]]
-        for num in nums[1:]:
-            prefix_ones.append(prefix_ones[-1] + num)
-
-        deltas = {0: -1}
+        
         max_len = 0
+        seen_deltas = {0: -1}
+        cur_delta = 0
         for pos, num in enumerate(nums):
-            ones = prefix_ones[pos]
-            zeroes = pos + 1 - ones
-            delta = zeroes - ones
-            if delta in deltas:
-                max_len = max(max_len, pos - deltas[delta])
+            cur_delta += 2 * num - 1 
+
+            if cur_delta in seen_deltas:
+                max_len = max(max_len, pos - seen_deltas[cur_delta])
             else:
-                deltas[delta] = pos
+                seen_deltas[cur_delta] = pos
 
 
 
