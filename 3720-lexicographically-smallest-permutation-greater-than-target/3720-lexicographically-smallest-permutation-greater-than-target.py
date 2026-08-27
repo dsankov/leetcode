90998d649e92bf7a259cc58a49debc7336e8e0a1
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2026-08-27 17:33:53 +0300

    Time: 11 ms (60.53%), Space: 19.5 MB (42.11%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -1,72 +1,55 @@
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
-        n = len(s)
-
-        cnt = [0] * 26
-
-        for c in s:
-            cnt[ord(c) - ord('a')] += 1
-
-        # prefix = part that currently matches target
+        # n = len(s)
+        char_count = collections.Counter(s)
         prefix = []
 
-        for i in range(n):
-
-            x = ord(target[i]) - ord('a')
-
-            # If target[i] is unavailable,
-            # we cannot continue matching.
-            if cnt[x] == 0:
+        for target_char in target:
+            if char_count[target_char] == 0:
                 break
 
-            cnt[x] -= 1
-            prefix.append(target[i])
+            char_count[target_char] -= 1
+            prefix.append(target_char)
 
-        # If we stopped before matching the whole target (because the
-        # needed character ran out), first try placing a character
-        # strictly greater than target[i] right at that same position,
-        # using whatever counts are left (nothing was consumed here yet).
-        if len(prefix) < n:
-            i = len(prefix)
-            x = ord(target[i]) - ord('a')
+        if len(prefix) < len(target):
+            char_to_change = target[len(prefix)]
+            char_to_change_idx = ord(char_to_change) - ord("a")
 
-            for c in range(x + 1, 26):
-                if cnt[c] == 0:
+            for candidate_char in string.ascii_lowercase[char_to_change_idx + 1:]: 
+                if char_count[candidate_char] == 0:
                     continue
 
-                ans = "".join(prefix) + chr(ord('a') + c)
+                char_count[candidate_char] -= 1
+                suffix = []
+                for fill_char in string.ascii_lowercase:
+                    suffix.append(fill_char * char_count[fill_char])
 
-                cnt[c] -= 1
-
-                for ch in range(26):
-                    ans += chr(ord('a') + ch) * cnt[ch]
-
-                return ans
+                return "".join(prefix) + candidate_char + "".join(suffix)
 
         # Otherwise (or if that attempt failed), backtrack through the
         # matched prefix from right to left.
-        for i in range(len(prefix) - 1, -1, -1):
+
+        # for i in range(len(prefix) - 1, -1, -1):
+        for prefix_char in reversed(prefix):
 
             # Restore the character at position i.
-            cnt[ord(prefix[i]) - ord('a')] += 1
+            # cnt[ord(prefix[i]) - ord('a')] += 1
+            char_count[prefix_char] += 1
             prefix.pop()
 
-            x = ord(target[i]) - ord('a')
+            # x = ord(target[i]) - ord('a')
+            char_to_change_idx = ord(prefix_char) - ord("a")
 
             # Find smallest character > target[i].
-            for c in range(x + 1, 26):
-
-                if cnt[c] == 0:
+            for candidate_char in string.ascii_lowercase[char_to_change_idx + 1:]: 
+                if char_count[candidate_char] == 0:
                     continue
 
-                ans = "".join(prefix) + chr(ord('a') + c)
+                char_count[candidate_char] -= 1
+                suffix = []
+                for fill_char in string.ascii_lowercase:
+                    suffix.append(fill_char * char_count[fill_char])
 
-                cnt[c] -= 1
-
-                # Fill remaining characters in sorted order.
-                for ch in range(26):
-                    ans += chr(ord('a') + ch) * cnt[ch]
-
-                return ans
+                return "".join(prefix) + candidate_char + "".join(suffix)
 
         return ""
