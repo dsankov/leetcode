85db457e57a481021d8a3103655c054df01557e5
Dmitry Sankov <71551209+dsankov@users.noreply.github.com>
2026-08-27 17:38:50 +0300

    Time: 3 ms (97.37%), Space: 19.4 MB (42.11%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -1,20 +1,18 @@
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
-        # n = len(s)
         char_count = collections.Counter(s)
         prefix = []
 
         for target_char in target:
             if char_count[target_char] == 0:
                 break
-
             char_count[target_char] -= 1
             prefix.append(target_char)
 
         if len(prefix) < len(target):
             char_to_change = target[len(prefix)]
+            
             char_to_change_idx = ord(char_to_change) - ord("a")
-
             for candidate_char in string.ascii_lowercase[char_to_change_idx + 1:]: 
                 if char_count[candidate_char] == 0:
                     continue
@@ -26,21 +24,12 @@ class Solution:
 
                 return "".join(prefix) + candidate_char + "".join(suffix)
 
-        # Otherwise (or if that attempt failed), backtrack through the
-        # matched prefix from right to left.
-
-        # for i in range(len(prefix) - 1, -1, -1):
+       
         for prefix_char in reversed(prefix):
-
-            # Restore the character at position i.
-            # cnt[ord(prefix[i]) - ord('a')] += 1
             char_count[prefix_char] += 1
             prefix.pop()
 
-            # x = ord(target[i]) - ord('a')
             char_to_change_idx = ord(prefix_char) - ord("a")
-
-            # Find smallest character > target[i].
             for candidate_char in string.ascii_lowercase[char_to_change_idx + 1:]: 
                 if char_count[candidate_char] == 0:
                     continue
